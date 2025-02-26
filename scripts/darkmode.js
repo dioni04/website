be5708fffd2c8d66b71dae2c70850712237f6cd2
dioni <dioni@Win11.>
2025-02-26 15:51:04 -0300

    260225

--- scripts/darkmode.js
+++ scripts/darkmode.js
@@ -1,10 +1,30 @@
+function switchMode(icon, monoIcons){
+    document.body.classList.toggle("dark-mode");
+    icon.classList.toggle("invert-icon");
+    monoIcons.forEach(img => {
+        img.classList.toggle("invert-icon");
+    });
+
+    // Save mode preference to localStorage
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("dark-mode", "enabled");
+    } else {
+        localStorage.setItem("dark-mode", "disabled");
+    }
+}
+
+
 document.addEventListener('DOMContentLoaded', () => {
     const colorScheme = document.getElementById('dark-mode-button');
     const icon = document.getElementById('icon-switcher');
+    const monoIcons = document.querySelectorAll('.footer-box img');
+
     if(colorScheme && icon){
+        if(localStorage.getItem("dark-mode") === "enabled"){
+            switchMode(icon, monoIcons);
+        }
         colorScheme.addEventListener('click', () => {
-            document.body.classList.toggle("dark-mode");
-            icon.classList.toggle("invert-icon");
+            switchMode(icon, monoIcons);
         });
     }
 });
